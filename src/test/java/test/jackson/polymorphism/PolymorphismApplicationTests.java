241ac0c793c3786cd4c55d7462655e3090368a22
Maurizio Casciano <mauriziocasciano7@gmail.com>
2022-01-26 15:39:23 +0100

    Spring Initializer
    https://start.spring.io/#!type=maven-project&language=java&platformVersion=2.6.3&packaging=jar&jvmVersion=17&groupId=test.jackson&artifactId=polymorphism&name=polymorphism&description=Demo%20project%20for%20Spring%20Boot&packageName=test.jackson.polymorphism&dependencies=web,lombok

---
+++ src/test/java/test/jackson/polymorphism/PolymorphismApplicationTests.java
@@ -0,0 +1,13 @@
+package test.jackson.polymorphism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolymorphismApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
