438196ad10670a989f67b3a2e3c7a16b948e4e83
Maurizio Casciano <mauriziocasciano7@gmail.com>
2022-01-26 16:44:25 +0100

    Polymorphism Tests

--- src/test/java/test/jackson/polymorphism/PolymorphismApplicationTests.java
+++ src/test/java/test/jackson/polymorphism/PolymorphismApplicationTests.java
@@ -3,7 +3,8 @@ package test.jackson.polymorphism;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.platform.commons.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import test.jackson.polymorphism.domain.enumeration.Impact;
@@ -16,18 +17,19 @@ import java.util.UUID;
 
 @SpringBootTest
 @RequiredArgsConstructor
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class PolymorphismApplicationTests {
 
 	@Autowired
 	private ObjectMapper objectMapper;
+	private static Element ELEMENT;
+	private static String SERIALIZED_JSON;
+	private static Element DESERIALIZED_ELEMENT;
+	private static String DESERIALIZED_JSON;
 
-	@Test
-	void contextLoads() {
-	}
-
-	@Test
-	void testJacksonPolymorphism(){
-		Element<String> element = ElementModel.builder()
+	@BeforeAll
+	public static void init(){
+		ELEMENT = ElementModel.builder()
 				.id(UUID.randomUUID().toString())
 				.name("Element Name")
 				.description("Element Description")
@@ -36,10 +38,40 @@ class PolymorphismApplicationTests {
 						.impact(Impact.HIGH)
 						.build())
 				.build();
+	}
 
+	@Test
+	@Order(1)
+	public void testJacksonPolymorphismSerialization(){
 		try {
-			String JSON = this.objectMapper.writeValueAsString(element);
-			System.out.println(JSON);
+			SERIALIZED_JSON = this.objectMapper.writeValueAsString(ELEMENT);
+			Assertions.assertNotNull(SERIALIZED_JSON);
+			Assertions.assertTrue(StringUtils.isNotBlank(SERIALIZED_JSON));
+
+			System.out.println("Serialized JSON");
+			System.out.println(SERIALIZED_JSON);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	@Order(2)
+	public void testJacksonPolymorphismDeserialization(){
+		try {
+			DESERIALIZED_ELEMENT = this.objectMapper.readValue(SERIALIZED_JSON, Element.class);
+			Assertions.assertNotNull(DESERIALIZED_ELEMENT);
+
+			Assertions.assertEquals(ELEMENT, DESERIALIZED_ELEMENT);
+
+			DESERIALIZED_JSON = this.objectMapper.writeValueAsString(DESERIALIZED_ELEMENT);
+			Assertions.assertNotNull(DESERIALIZED_JSON);
+			Assertions.assertTrue(StringUtils.isNotBlank(DESERIALIZED_JSON));
+
+			Assertions.assertEquals(SERIALIZED_JSON, DESERIALIZED_JSON);
+
+			System.out.println("Deserialized JSON");
+			System.out.println(DESERIALIZED_JSON);
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
 		}
